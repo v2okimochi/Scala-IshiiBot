493a2c25d463f71114a3a0609241d8c188292f32
Shunya Ishii <sh141ts@gmail.com>
2019-06-02 21:44:50 +0900

    token環境変数名を変更＆botの状態リストが増える度に古い状態を消すよう変更

--- src/main/scala/SetBot.scala
+++ src/main/scala/SetBot.scala
@@ -3,7 +3,7 @@ import slack.rtm.SlackRtmClient
 import scala.concurrent.ExecutionContextExecutor
 
 class SetBot {
-  val token: String = sys.env("SCALA_BOT_TOKEN_TEST")
+  val token: String = sys.env("SCALA_BOT_TOKEN")
   implicit val system: ActorSystem = ActorSystem("slack")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
   val client: SlackRtmClient = SlackRtmClient(token)
